255621d9d7d026e1de338ad2622d08f0ea97a026
Valeryi Savich <relrin78@gmail.com>
2018-03-10 14:23:02 +0300

    Implemented generating regex for dynamic URL

--- lib/endpoint/dynamic.ex
+++ lib/endpoint/dynamic.ex
@@ -4,15 +4,54 @@ defmodule Spotter.Endpoint.Dynamic do
   """
   use Spotter.Endpoint.Base
 
+  @good_match "[^{}\/.]+"
+  @dynamic_parameter ~r/({\s*[\w\d_-]+\s*})/
+  @valid_dynamic_parameter ~r/{(?P<var>[\w][\w\d_-]*)}/
+
   @doc """
   Defines the endpoint with dynamic path.
 
-  * :path - Path to the recource. For example, `api/learderboard/get/{id}`. Required.
+  * :path - Path to the recource. For example, `api.learderboard.get.{id}`. Required.
   * :regex - Regex expression for further checks with the passed path. Required.
   * :permissions - List of permissions, required for getting an access to the resource. Default is `[]`.
   """
   @enforce_keys [:path, :regex]
   defstruct [:path, :regex, permissions: []]
+
+  # Generates a regular expression for the dynamic parameter in URL if it was found. Otherwise
+  # will return a string as is.
+  @spec process_path_part(path::String.t) :: String.t
+  defp process_path_part(path) do
+    case Regex.match?(@valid_dynamic_parameter, path) do
+      true -> Enum.join(['(?P<', Regex.named_captures(@valid_dynamic_parameter, path)["var"], '>', @good_match, ")"], "")
+      false -> path
+    end
+  end
+
+  @doc """
+  Generates a regular expression based on the passed `path` string argument. Can raise the
+  Regex.CompileError in case of errors.
+  """
+  @spec generate_regex(path::String.t) :: String.t
+  def generate_regex(path) do
+    parsed_path = Regex.split(@dynamic_parameter, path, include_captures: true)
+      |> Enum.map(&(process_path_part(&1)))
+      |> Enum.join("")
+    regex = Enum.join(['^', parsed_path, '$'], "")
+    Regex.compile!(regex)
+  end
+
+  @doc """
+  Returns a new instance of Spotter.Endpoint.Dynamic struct.
+  """
+  @spec new(path::String.t, permissions::[String.t]) :: Spotter.Endpoint.Dynamic
+  def new(path, permissions) do
+    %Spotter.Endpoint.Dynamic{
+      path: path,
+      regex: generate_regex(path),
+      permissions: permissions
+    }
+  end
 
   @doc """
   Checking a match of the passed path with the endpoint path via regex search.
--- test/endpoint/dynamic_test.exs
+++ test/endpoint/dynamic_test.exs
@@ -2,80 +2,56 @@ defmodule SpotterEndpointDynamicTest do
   use ExUnit.Case
 
   test "Spotter.Endpoint.Dynamic contructor set permissions to empty list" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new("api.leaderboard.get.{user_id}", [])
 
     assert endpoint.path == "api.leaderboard.get.{user_id}"
-    assert endpoint.regex == ~r/^api.leaderboard.get.(?P<var>[^{}.]+)$/
+    assert endpoint.regex == ~r/^api.leaderboard.get.(?P<user_id>[^{}\/.]+)$/
     assert endpoint.permissions == []
   end
 
   test "Spotter.Endpoint.Dynamic contructor with custom permissions" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled,
-      permissions: ["api.leaderboard.get", ]
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new(
+      "api.leaderboard.get.{user_id}",
+      ["api.leaderboard.get"]
+    )
 
     assert endpoint.path == "api.leaderboard.get.{user_id}"
-    assert endpoint.regex == ~r/^api.leaderboard.get.(?P<var>[^{}.]+)$/
+    assert endpoint.regex == ~r/^api.leaderboard.get.(?P<user_id>[^{}\/.]+)$/
     assert endpoint.permissions == ["api.leaderboard.get", ]
   end
 
   test "Spotter.Endpoint.Dynamic.match returns true for exact match" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled,
-      permissions: ["api.leaderboard.get", ]
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new(
+      "api.leaderboard.get.{user_id}",
+      ["api.leaderboard.get", ]
+    )
 
     assert Spotter.Endpoint.Dynamic.match(endpoint, "api.leaderboard.get.user-123456789")
   end
 
   test "Spotter.Endpoint.Dynamic.match returns false for not matched path" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled,
-      permissions: ["api.leaderboard.get", ]
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new(
+      "api.leaderboard.get.{user_id}",
+      ["api.leaderboard.get", ]
+    )
 
     assert not Spotter.Endpoint.Plain.match(endpoint, "some.another.api")
   end
 
   test "Spotter.Endpoint.Dynamic.has_permission returns true for correct permissions" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled,
-      permissions: ["get", "update"]
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new("api.leaderboard.get.{user_id}", ["get", "update"])
 
     assert Spotter.Endpoint.Plain.has_permission(endpoint, ["get", "update", "delete"])
   end
 
   test "Spotter.Endpoint.Dynamic.has_permission returns false for invalid permissions" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled,
-      permissions: ["list", "delete"]
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new("api.leaderboard.get.{user_id}", ["list", "delete"])
 
     assert not Spotter.Endpoint.Plain.has_permission(endpoint, ["list", "update"])
   end
 
   test "Spotter.Endpoint.Dynamic.has_permission returns true for the endpoint without permissions" do
-    {:ok, regex_compiled} = Regex.compile("^api.leaderboard.get.(?P<var>[^{}.]+)$")
-    endpoint = %Spotter.Endpoint.Dynamic{
-      path: "api.leaderboard.get.{user_id}",
-      regex: regex_compiled
-    }
+    endpoint = Spotter.Endpoint.Dynamic.new("api.leaderboard.get.{user_id}", [])
 
     assert Spotter.Endpoint.Plain.has_permission(endpoint, ["get", "patch", "head"])
   end
